f913dddb76dda5f30ab787796c93cb66d261e935
gitgitWi <wiii@kakao.com>
2022-09-23 16:19:21 +0000

    build (deno) fresh 자동생성 파일

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,20 +2,22 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import config from './deno.json' assert { type: 'json' };
+import * as $0 from './routes/[name].tsx';
+import * as $1 from './routes/api/treemap/api/be.ts';
+import * as $2 from './routes/index.tsx';
+import * as $$0 from './islands/Counter.tsx';
+import * as $$1 from './islands/RefreshButton.tsx';
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    './routes/[name].tsx': $0,
+    './routes/api/treemap/api/be.ts': $1,
+    './routes/index.tsx': $2,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
+    './islands/Counter.tsx': $$0,
+    './islands/RefreshButton.tsx': $$1,
   },
   baseUrl: import.meta.url,
   config,
