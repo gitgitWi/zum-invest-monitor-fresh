7cadb524caa1d086d07474379bc6301b7b2651f3
gitgitWi <wiii@kakao.com>
2022-09-23 16:17:06 +0000

    feat (routes/apis) 국내 증시맵 fetch api 추가

---
+++ routes/api/treemap/api/be.ts
@@ -0,0 +1,28 @@
+import { Handlers } from '$fresh/server.ts';
+import { Market$Kr } from '../../../../constants/apis/common.ts';
+
+import { treemapRealtimeApis } from '../../../../constants/apis/index.ts';
+
+/**
+ * - /api/treemap/api/be?market={kospi|kosdaq}
+ */
+export const handler: Handlers = {
+  async GET(req) {
+    const { searchParams } = new URL(req.url);
+
+    const market = searchParams.get('market') || '';
+    if (!market || !(market in treemapRealtimeApis)) {
+      return new Response(JSON.stringify({ data: 'INVALID market name' }), {
+        status: 404,
+      });
+    }
+
+    const data = await fetch(treemapRealtimeApis[market as Market$Kr]).then((res) =>
+      res.json()
+    );
+
+    return new Response(
+      JSON.stringify(data),
+    );
+  },
+};
