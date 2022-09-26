d7ba2dc89ec86d7bd81082d02085f1b9067b4fc1
gitgitWi <wiii@kakao.com>
2022-09-26 07:30:35 +0000

    feat (component/select) category selector 컴포넌트 구현

---
+++ components/molecules/category-selector/CategorySelector.tsx
@@ -0,0 +1,33 @@
+import { JSX } from 'preact/jsx-runtime';
+
+import { SelectorOption, SelectorOptionProps } from '../../atoms/index.ts';
+import { Market$Kr } from '../../../constants/apis/common.ts';
+
+interface CategorySelectorProps extends JSX.HTMLAttributes<HTMLSelectElement> {
+  options: SelectorOptionProps[];
+  selectOption: (marketName: Market$Kr) => void;
+}
+
+export function CategorySelector(
+  { options, selectOption }: CategorySelectorProps,
+) {
+  const onSelectChange = (e: Event) => {
+    // @ts-ignore
+    selectOption(e.currentTarget?.value || '');
+  };
+
+  return (
+    <select
+      class='w-max min-w-[80px] py-1 px-2 rounded-2xl text-center text-xl font-bold'
+      onChange={onSelectChange}
+      value={options[0].value}
+    >
+      {options.map(({ value, name }) => (
+        <SelectorOption
+          value={value}
+          name={name}
+        />
+      ))}
+    </select>
+  );
+}
--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,1 +1,2 @@
 export * from './card-code/index.ts';
+export * from './category-selector/index.ts'
