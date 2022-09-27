5cbfaf1d523cf73fbbc772b16990c3379bc79e7e
gitgitWi <wiii@kakao.com>
2022-09-27 15:25:42 +0000

    style (components/header) bgColor 변경

--- components/common/header/Header.tsx
+++ components/common/header/Header.tsx
@@ -2,7 +2,7 @@ import { Title } from '@/components/atoms/mod.ts';
 
 export function Header() {
   return (
-    <header class="w-full p-1.5 bg-blue-900">
+    <header class="w-full p-1.5 bg-blue-700">
       <Title text="Watch InvestZUM" href="/" />
     </header>
   );
