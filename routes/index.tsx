381511063448a23163c6af2d7a4a86ff7fb60f03
gitgitWi <wiii@kakao.com>
2022-09-27 12:27:48 +0000

    feat (components) header/title 등 스타일 수정

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,6 +13,9 @@ export default function Home() {
       <body class="w-full h-screen flex flex-col items-center">
         <Header />
         <main class="w-full h-full mb-10 flex flex-col items-center justify-center">
+          <h2 className="pb-4 font-extrabold text-3xl align-middle text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500 ">
+            Validator Pages
+          </h2>
           {validatorPageRoutes.map(({ name, to }) => (
             <button class="w-1/3 my-2 p-2 border-2 border-blue-500 rounded-3xl font-medium hover:bg-blue-400 hover:text-white hover:font-extrabold duration-100 ">
               <a href={to}>{name}</a>
