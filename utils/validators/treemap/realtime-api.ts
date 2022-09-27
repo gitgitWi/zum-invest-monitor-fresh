7cafb63ef584b2849558f2b34d5a8d33a7516030
gitgitWi <wiii@kakao.com>
2022-09-27 07:33:16 +0000

    feat (validator/treemap) 비정상 Property 갖는 종목 검사 함수 추가

--- utils/validators/treemap/realtime-api.ts
+++ utils/validators/treemap/realtime-api.ts
@@ -1,14 +1,23 @@
-import { Treemap$Sector$RealtimeApi } from '@/types/treemap/mod.ts';
+import {
+  Treemap$Sector$RealtimeApi,
+  Treemap$Stock$RealtimeApi,
+} from '@/types/treemap/mod.ts';
 import { ValidatorResult } from '@/types/validator/mod.ts';
 
 export const getSectorsNumber = (
   apiData: Treemap$Sector$RealtimeApi[]
 ): ValidatorResult => {
-  const sectorIds = apiData.map(({ industryId }) => industryId);
+  const sectors = apiData
+    .map(({ industryId, industryName }) => ({
+      industryId,
+      industryName,
+    }))
+    .sort((a, b) => a.industryId - b.industryId);
+  const sectorIds = sectors.map(({ industryId }) => industryId);
 
   return {
     isValid: sectorIds.length === new Set(sectorIds).size,
-    values: { sectorIds, sectorIdLength: sectorIds.length },
+    values: { sectors, sectorIdLength: sectors.length },
     message: '중복 업종 확인',
   };
 };
@@ -16,13 +25,83 @@ export const getSectorsNumber = (
 export const getStocksNumber = (
   apiData: Treemap$Sector$RealtimeApi[]
 ): ValidatorResult => {
-  const stockIds = apiData
-    .map(({ stocks }) => stocks.map(({ stockId }) => stockId))
-    .flat(Infinity);
+  const stocks = apiData
+    .map(({ stocks }) =>
+      stocks.map(({ stockId, stockName, fluctuationRate }) => ({
+        stockId,
+        stockName,
+        fluctuationRate,
+      }))
+    )
+    .flat(2)
+    .sort((a, b) => a.stockId.localeCompare(b.stockId));
+
+  const stockIds = stocks.map(({ stockId }) => stockId);
 
   return {
     isValid: stockIds.length === new Set(stockIds).size,
-    values: { stockIds, stockIdLength: stockIds.length },
+    values: { stocks, stockIdLength: stockIds.length },
     message: '중복 종목 확인',
+  };
+};
+
+export const getStocksHaveInvalidProp = (
+  apiData: Treemap$Sector$RealtimeApi[]
+): ValidatorResult => {
+  const invalidStocks: Partial<Treemap$Stock$RealtimeApi>[] = apiData
+    .map(({ stocks }) =>
+      stocks.map(
+        ({
+          stockId,
+          stockName,
+          currentPrice,
+          fluctuationRate,
+          fluctuationPrice,
+          tradingVolume,
+          totalValue,
+        }) => ({
+          stockId,
+          stockName,
+          currentPrice,
+          fluctuationRate,
+          fluctuationPrice,
+          tradingVolume,
+          totalValue,
+        })
+      )
+    )
+    .flat(2)
+    .filter(
+      ({
+        stockId,
+        stockName,
+        currentPrice,
+        fluctuationRate,
+        fluctuationPrice,
+        tradingVolume,
+        totalValue,
+      }) => {
+        if (!stockId || !stockName) return true;
+
+        if (
+          typeof currentPrice !== 'number' ||
+          typeof fluctuationRate !== 'number' ||
+          typeof fluctuationPrice !== 'number' ||
+          typeof tradingVolume !== 'number' ||
+          typeof totalValue !== 'number' ||
+          currentPrice <= 0 ||
+          tradingVolume < 0 ||
+          totalValue <= 0
+        )
+          return true;
+
+        return false;
+      }
+    );
+
+  return {
+    isValid: invalidStocks.length === 0,
+    values: { invalidStocks },
+    message: '비정상 값 들어있는 종목 확인',
   };
 };
