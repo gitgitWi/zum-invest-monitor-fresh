9cf48c47120efd14751fc8a285c266b16b1cac0f
gitgitWi <wiii@kakao.com>
2022-09-27 11:38:27 +0000

    feat (component/header) index로 가는 링크 추가

--- components/atoms/title/Title.tsx
+++ components/atoms/title/Title.tsx
@@ -1,7 +1,14 @@
 interface TitleProps {
   text: string;
+  href: string;
 }
 
-export function Title({ text }: TitleProps) {
-  return <h1 class='w-full text-4xl text-white font-bold text-center'>{text}</h1>;
+export function Title({ text, href }: TitleProps) {
+  return (
+    <h1 class="w-full text-4xl text-white font-bold text-center">
+      <a class="" href={href}>
+        {text}
+      </a>
+    </h1>
+  );
 }
--- components/common/header/Header.tsx
+++ components/common/header/Header.tsx
@@ -3,7 +3,7 @@ import { Title } from '@/components/atoms/mod.ts';
 export function Header() {
   return (
     <header class="w-full py-2 bg-blue-900">
-      <Title text="Watch InvestZUM" />
+      <Title text="Watch InvestZUM" href="/" />
     </header>
   );
 }
