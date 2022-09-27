816515c90c52229e3c198fa70f8be5e1191d5062
gitgitWi <wiii@kakao.com>
2022-09-27 11:51:06 +0000

    feat (component/header) `select-none` 추가

--- components/atoms/title/Title.tsx
+++ components/atoms/title/Title.tsx
@@ -5,8 +5,8 @@ interface TitleProps {
 
 export function Title({ text, href }: TitleProps) {
   return (
-    <h1 class="w-full text-4xl text-white font-bold text-center">
-      <a class="" href={href}>
+    <h1 class="w-full text-4xl text-white font-bold text-center select-none">
+      <a class="select-none" href={href}>
         {text}
       </a>
     </h1>
