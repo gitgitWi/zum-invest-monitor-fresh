72bcd987b2d1491036a1a1997ebb75a0d2b159b2
gitgitWi <wiii@kakao.com>
2022-09-27 13:01:04 +0000

    feat (pages/index) 스타일 수정, 링크 button 태그 제거하고 a 태그만 사용

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,6 +3,16 @@ import { Header } from '@/components/common/header/mod.ts';
 import { Footer } from '@/components/common/footer/mod.ts';
 import { validatorPageRoutes } from '../constants/routes.ts';
 
+const styles = {
+  body: 'w-full h-screen flex flex-col items-center',
+  main: 'w-full h-full mb-10 flex flex-col items-center justify-center',
+  pagesHeader:
+    'pb-4 font-extrabold text-3xl align-middle text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500',
+  pageLink:
+    'w-56 my-2 px-1 py-2 rounded-3xl font-medium bg-transparent border-b-2 border-gray-500 text-center \
+    hover:bg-blue-400 hover:text-white hover:font-bold hover:italic hover:border-blue-500 duration-100',
+};
+
 export default function Home() {
   return (
     <>
@@ -10,18 +20,14 @@ export default function Home() {
         title="Watch Invest ZUM"
         description="watch && validate Invest ZUM API"
       />
-      <body class="w-full h-screen flex flex-col items-center">
+      <body class={styles.body}>
         <Header />
-        <main class="w-full h-full mb-10 flex flex-col items-center justify-center">
-          <h2 className="pb-4 font-extrabold text-3xl align-middle text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500 ">
-            Validator Pages
-          </h2>
+        <main class={styles.main}>
+          <h2 class={styles.pagesHeader}>Validator Pages</h2>
           {validatorPageRoutes.map(({ krName, to }) => (
-            <button class="w-1/2 sm:w-1/4 my-2 px-1 py-2 rounded-3xl font-medium bg-transparent border-b-2 border-gray-500 hover:bg-blue-400 hover:text-white hover:font-bold hover:italic hover:border-blue-500 duration-100">
-              <a href={to} class="w-full p-2 ">
-                {krName}
-              </a>
-            </button>
+            <a href={to} class={styles.pageLink}>
+              {krName}
+            </a>
           ))}
         </main>
         <Footer />
