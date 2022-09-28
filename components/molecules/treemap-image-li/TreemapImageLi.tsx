d16f6beef3f3c3b9945384ce06c2e88685aadcfb
gitgitWi <wiii@kakao.com>
2022-09-28 13:34:49 +0000

    style (component/treemap-image-li) 내부 스타일 추가

--- components/molecules/treemap-image-li/TreemapImageLi.tsx
+++ components/molecules/treemap-image-li/TreemapImageLi.tsx
@@ -3,12 +3,24 @@ interface TreemapImageProps {
   imgSrc: string;
 }
 
+const styles = {
+  li: 'my-2 p-4 rounded-xl flex flex-col bg-gray-100',
+  title: 'font-bold select-none',
+  url: 'text-xs whitespace-pre-wrap my-2',
+  anchor:
+    'font-italic font-mono text-underline \
+    hover:text-blue-700 hover:font-semibold duration-100',
+};
+
 export function TreemapImageLi({ title, imgSrc }: TreemapImageProps) {
   return (
-    <li class="flex flex-col">
-      <p class="">{title}</p>
-      <p class="">
-        url: <u class="">{imgSrc}</u>
+    <li class={styles.li}>
+      <p class={styles.title}>{title}</p>
+      <p class={styles.url}>
+        url:{' '}
+        <a href={imgSrc} target="_blank" class={styles.anchor}>
+          {imgSrc}
+        </a>
       </p>
       <img src={imgSrc} alt="" />
     </li>
