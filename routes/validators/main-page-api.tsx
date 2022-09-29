c7685b1c8f07fee07e816e95b8290f8c03471496
gitgitWi <wiii@kakao.com>
2022-09-29 19:56:32 +0900

    style (footer) 메인페이지 외 나머지 페이지 footer 제거

--- routes/validators/main-page-api.tsx
+++ routes/validators/main-page-api.tsx
@@ -1,6 +1,5 @@
 import { $Head } from '@/components/common/head/mod.ts';
 import { Header } from '@/components/common/header/mod.ts';
-import { Footer } from '@/components/common/footer/mod.ts';
 
 import HomeApiValidator from '@/islands/HomeApiValidator.tsx';
 
@@ -15,7 +14,6 @@ export default function MainNewsPage() {
       <main class="w-full h-full pb-16 flex flex-col items-center">
         <HomeApiValidator />
       </main>
-      <Footer />
     </>
   );
 }
