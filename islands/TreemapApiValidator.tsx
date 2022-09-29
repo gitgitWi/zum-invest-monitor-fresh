6374c5490957b46c2f0698dafdfbc55c5672f8e6
gitgitWi <wiii@kakao.com>
2022-09-29 20:20:30 +0900

    refactor (component/api-link) 페이지에 컴포넌트 적용

--- islands/TreemapApiValidator.tsx
+++ islands/TreemapApiValidator.tsx
@@ -9,7 +9,7 @@ import {
   treemapRealtimeApis,
   treemapRealtimeApisClient,
 } from '@/constants/apis/mod.ts';
-import { type SelectorOptionProps } from '@/components/atoms/mod.ts';
+import { type SelectorOptionProps, ApiLink } from '@/components/atoms/mod.ts';
 import {
   CardCode,
   CategorySelector,
@@ -29,9 +29,7 @@ const styles = {
   container: 'mt-4 p-4 pt-0 w-full max-w-5xl flex flex-col items-start',
   titleContainer: 'w-full flex justify-start items-center',
   subtitle: 'w-max pr-2 font-bold text-2xl',
-  description: 'flex justify-start items-center pb-2',
-  apiLink:
-    'pr-2 text-base font-italic font-mono text-sm sm:text-base text-underline text-indigo-900',
+  description: 'w-full flex justify-start items-center pb-2',
   codeCard: 'relative w-full flex items-start',
   codeCardHeightToggleButton:
     'absolute w-6 h-6 right-6 top-3 rounded-full duration-500 cursor-pointer',
@@ -59,20 +57,20 @@ const setQueryStringMarket = (marketName: Market$Kr) => {
 
 export default function TreemapApiValidator() {
   const marketQuery = new URLSearchParams(
-    IS_BROWSER ? location.search : ''
+    IS_BROWSER ? location.search : '',
   ).get('market');
 
   const [treemapMarketOption, setTreemapMarketOption] =
     useState<SelectorOptionProps>(
       categoryOptions.find(({ value }) => marketQuery === value) ||
-        categoryOptions[0]
+        categoryOptions[0],
     );
   const [apiData, setApiData] = useState<
     Record<string, unknown> | unknown[] | ''
   >('');
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [validatorResults, setValidatorResults] = useState<ValidatorResult[]>(
-    []
+    [],
   );
 
   const [isApiResultHeightFull, toggleIsApiResultHeightFull] =
@@ -100,7 +98,7 @@ export default function TreemapApiValidator() {
   const selectTreemapOption = (marketName: Market$Kr) => {
     setTreemapMarketOption(
       categoryOptions.find(({ value }) => value === marketName) ||
-        categoryOptions[0]
+        categoryOptions[0],
     );
   };
 
@@ -128,13 +126,9 @@ export default function TreemapApiValidator() {
       </div>
 
       <div class={styles.description}>
-        <a
-          class={styles.apiLink}
-          href={treemapRealtimeApis[treemapMarketOption.value as Market$Kr]}
-          target="_blank"
-        >
-          {treemapRealtimeApis[treemapMarketOption.value as Market$Kr]}
-        </a>
+        <ApiLink
+          link={treemapRealtimeApis[treemapMarketOption.value as Market$Kr]}
+        />
         <RefreshButton
           buttonClickHandler={fetchTreemapApi}
           isLoading={isLoading}
